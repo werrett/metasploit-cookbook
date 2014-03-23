0d3894c25b66a935b9c4ce176d0b5155494bb0a9
Jonathan Werrett <jonathan@werrett.com.au>
2014-03-23 23:33:14 +0800

    Updated license details.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'metasploit'
 maintainer       'Jonathan Werrett'
 maintainer_email 'jonathan@werrett.hk'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs and Configures Metasploit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
