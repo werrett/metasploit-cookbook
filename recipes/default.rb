864d6e190071faf3ae8099d95baa396574df66b2
Jonathan Werrett <jonathan@werrett.com.au>
2014-03-23 23:10:15 +0800

    Added Metasploit to PATH for all users. Tidied up recipe.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,37 +2,34 @@
 # Cookbook Name:: metasploit
 # Recipe:: default
 #
-# Copyright (C) 2013 Jonathan Werrett
-# 
-# All rights reserved - Do Not Redistribute
+# Copyright (C) 2014 Jonathan Werrett
 #
 
-package 'build-essential'
-package 'zlib1g'
-package 'zlib1g-dev'
-package 'libxml2'
-package 'libxml2-dev'
-package 'libxslt-dev'
-package 'locate'
-package 'libreadline6-dev'
-package 'libcurl4-openssl-dev'
-package 'git-core'
-package 'libssl-dev'
-package 'libyaml-dev'
-package 'openssl'
 package 'autoconf'
-package 'libtool'
-package 'ncurses-dev'
 package 'bison'
 package 'curl'
-package 'wget'
-package 'postgresql'
-package 'postgresql-contrib'
-package 'libpq-dev'
+package 'git-core'
 package 'libapr1'
 package 'libaprutil1'
-package 'libsvn1'
+package 'libcurl4-openssl-dev'
 package 'libpcap-dev'
+package 'libpq-dev'
+package 'libreadline6-dev'
+package 'libssl-dev'
+package 'libsvn1'
+package 'libtool'
+package 'libxml2-dev'
+package 'libxml2'
+package 'libxslt-dev'
+package 'libyaml-dev'
+package 'locate'
+package 'ncurses-dev'
+package 'openssl'
+package 'postgresql-contrib'
+package 'postgresql'
+package 'wget'
+package 'zlib1g-dev'
+package 'zlib1g'
 
 metasploit_directory = node['metasploit']['directory'] || "/opt/metasploit"
 github_url = node['metasploit']['github']['url']
@@ -47,6 +44,13 @@ end
 
 rbenv_root = node['rbenv']['root_path']
 
+template "/etc/profile.d/metasploit.sh" do
+  source 'metasploit.sh.erb'
+  owner 'root'
+  group 'root'
+  mode  '0755'
+end
+
 bash "install_metasploit" do
   cwd  metasploit_directory
   user "root"
@@ -55,16 +59,11 @@ bash "install_metasploit" do
     # Install pg before the bundle. Fixes a bug where pg_config was not found.
     RBENV_ROOT="#{rbenv_root}" && \
     RBENV_VERSION=`cat #{rbenv_root}/version` && \
-    $RBENV_ROOT/shims/gem install pg -q -v '0.16.0' --no-rdoc --no-ri -- --with-pg-config=/usr/bin/pg_config
+    $RBENV_ROOT/shims/gem install pg -q --no-rdoc --no-ri -- --with-pg-config=/usr/bin/pg_config
 
-    bundle install
+    $RBENV_ROOT/shims/bundle install
   EOH
   action :nothing
 
   not_if "#{metasploit_directory}msfconsole --version | grep -q 'Framework Version'"
 end
-
-
-
-
-
