0814e1676eea82e726357c5af3e956fd9ce65193
Jonathan Werrett <jonathan@werrett.com.au>
2014-09-06 12:32:42 +0800

    Added explict dependencies on ruby_build and rbenv cookbooks

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,6 @@ license          'MIT'
 description      'Installs and Configures Metasploit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
+
+depends "ruby_build"
+depends "rbenv"
